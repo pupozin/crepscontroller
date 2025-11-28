99256dece44b3c684e35b0f4011b0bb34f986e06
pupozin <pupogabriel88@gmail.com>
2025-11-28 16:46:32 -0300

    Feat: Switching for dotnet 8.0

--- CrepeControladorApi/Program.cs
+++ CrepeControladorApi/Program.cs
@@ -26,6 +26,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
